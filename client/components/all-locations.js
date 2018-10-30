532a404269ff7b40f10105b905c71b595480887a
Melissa Sage Dumont <melissadumont91@gmail.com>
2018-10-30 12:39:57 -0400

    Added API routes for filtering. Started a React component to display all routes and filtered routes.

---
+++ client/components/all-locations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class AllLocations extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return <div>hello world!</div>
+  }
+}
