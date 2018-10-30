b0b0e2cb547014240ad325329f3b4ab677e4f3d6
Samantha Francis <samfran239@yahoo.com>
2018-10-30 12:27:46 -0400

    grabbing locations

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const db = require('../server/db');
-const { User } = require('../server/db/models');
+const { User, Location } = require('../server/db/models');
 
 async function seed() {
 	await db.sync({ force: true });
@@ -19,15 +19,16 @@ async function seed() {
 			name: 'Hank Horrible',
 			email: 'murphy@email.com',
 			password: '234',
-			role: 'seller',
+			role: 'buyer',
 			image:
 				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3EO40pPzRET-MS570WOt-4i0K4AB2FgJh5eyH39sWa1huRgUjmw'
 		}),
 		User.create({
-			name: 'Earl Jackson',
-			email: 'Earl@jackson.com',
+			name: 'Davey Jones',
+			email: 'Davey@jones.com',
 			password: '456',
-			role: 'buyer',
+			role: 'seller',
+			isAdmin: true,
 			image:
 				'https://static01.nyt.com/images/2017/01/11/blogs/11-lens-maurice-slide-XQMQ/11-lens-maurice-slide-XQMQ-superJumbo.jpg'
 		}),
@@ -35,6 +36,7 @@ async function seed() {
 			name: 'Carmelita Souls',
 			email: 'Carmel@apple.com',
 			password: '567',
+			isAdmin: true,
 			role: 'seller',
 			image:
 				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5M2CFBdJ00cWFPZv4LawshY7l9ZGRsiMDMc5zhGeUC2iyt9uz4Q'
@@ -56,6 +58,16 @@ async function seed() {
 				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTik9xOrLHcsoFbkRt_Cyy0HOsxBZKOoANm_hWGhWp9z15fxa9I6Q'
 		})
 	]);
+
+	// const locations = await Promise.all([
+	// 	locations.create({
+	// 		address:,
+	// 		category:,
+	// 		quantity:,
+	// 		price:,
+	// 		imgGallery:
+	// 	})
+	// ])
 
 	console.log(`seeded ${users.length} users`);
 	console.log(`seeded successfully`);
