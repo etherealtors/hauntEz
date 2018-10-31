39d7adebff1d279cb449b452ac8bc3f53be9ef90
Melissa Sage Dumont <melissadumont91@gmail.com>
2018-10-31 09:14:41 -0400

    Finished initial work on all-locations.js, including filtering via dropdown menu.

--- client/components/all-locations.js
+++ client/components/all-locations.js
@@ -1,19 +1,44 @@
 import {connect} from 'react-redux'
 import React from 'react'
-import {getAllLocations} from '../store'
+import {getAllLocations, getFilteredLocations} from '../store'
 
 class AllLocations extends React.Component {
+  constructor() {
+    super()
+    this.changeFilter = this.changeFilter.bind(this)
+  }
+
   componentDidMount() {
     this.props.getAllLocations()
+  }
+
+  changeFilter(event) {
+    if (event.target.value === 'All') {
+      this.props.getAllLocations()
+    } else {
+      this.props.getFilteredLocations(event.target.value)
+    }
   }
 
   render() {
     return (
       <div>
         <div className="dropdown">
-          <div className="dropdown-content">
-            All Shack House Apartment Manse Castle Boat
-          </div>
+          <select>
+            {[
+              'All',
+              'Shack',
+              'House',
+              'Apartment',
+              'Manse',
+              'Castle',
+              'Boat'
+            ].map(type => (
+              <option key={type} value={type} onClick={this.changeFilter}>
+                {type}
+              </option>
+            ))}
+          </select>
         </div>
         <div>
           {this.props.locations.map(location => (
@@ -33,7 +58,8 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  getAllLocations: () => dispatch(getAllLocations())
+  getAllLocations: () => dispatch(getAllLocations()),
+  getFilteredLocations: category => dispatch(getFilteredLocations(category))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllLocations)
