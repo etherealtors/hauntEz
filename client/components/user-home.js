51f24534fa7866dbe967bce298b1627ff1f20484
Melissa Sage Dumont <melissadumont91@gmail.com>
2018-11-02 14:36:21 -0400

    Fixed merge conflicts

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,73 +1,44 @@
-<<<<<<< HEAD
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import AllLocations from './all-locations'
-=======
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import AllLocations from './all-locations';
-import { Link } from 'react-router-dom';
->>>>>>> 1596cd0dc75c5952158aefb22bd55f6ac4e9bce5
+import {Link} from 'react-router-dom'
 
 /**
  * COMPONENT
  */
-<<<<<<< HEAD
 export const UserHome = props => {
-  const {email} = props
-  console.log('asdfas', props)
+  const {email, isAdmin} = props
+
   return (
     <div>
       <h1 className="creepyFont" id="centered">
         Welcome to HauntEz!
       </h1>
-      {/*Admin: render + button top right of page to add products*/}
-      {/*render dropdown for categories*/}
-      {/*render all locations as an image grid*/}
-      {/*render edit button next to each image*/}
+
+      {/*ADMIN ONLY: Render + button top right of page to add products*/}
+      {isAdmin ? (
+        <Link to="/addLocation">
+          <h1>+</h1>
+        </Link>
+      ) : (
+        <div />
+      )}
+
       <AllLocations />
     </div>
   )
 }
-=======
-export const UserHome = (props) => {
-	const { email, isAdmin } = props;
-
-	return (
-		<div>
-			<h1 className="creepyFont" id="centered">
-				Welcome to HauntEz!
-			</h1>
-			
-			{/*ADMIN ONLY: Render + button top right of page to add products*/}
-			{(isAdmin) ? <Link to='/addLocation'><h1>+</h1></Link> : <div />}
-				
-			<AllLocations />
-		</div>
-	);
-};
->>>>>>> 1596cd0dc75c5952158aefb22bd55f6ac4e9bce5
 
 /**
  * CONTAINER
  */
-<<<<<<< HEAD
 const mapState = state => {
   return {
-    email: state.user.email
-    //get locations from state
+    email: state.user.email,
+    isAdmin: state.user.isAdmin
   }
 }
-=======
-const mapState = (state) => {
-	return {
-		email: state.user.email,
-		isAdmin: state.user.isAdmin
-	};
-};
->>>>>>> 1596cd0dc75c5952158aefb22bd55f6ac4e9bce5
 
 export default connect(mapState)(UserHome)
 
